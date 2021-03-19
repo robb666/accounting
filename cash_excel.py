8884401514b658802a703dafe128ebee4ece5f84
robb666 <robert.patryk.grzelak@gmail.com>
2021-03-19 10:44:53 +0100

    Update cash_excel.py

--- cash_excel.py
+++ cash_excel.py
@@ -23,6 +23,39 @@ except:
 
 
 
+
+
+
+tu = {'ALL': 'Allianz', 'AXA': 'AXA', 'COM': 'Compensa', 'EPZU': 'PZU', 'GEN': 'Generali', 'GOT': 'Gothaer',
+      'HDI': 'HDI', 'HES': 'Ergo Hestia', 'IGS': 'IGS', 'INT': 'INTER', 'LIN': 'LINK 4', 'MTU': 'MTU',
+      'PRO': 'Proama', 'PZU': 'PZU', 'RIS': 'InterRisk', 'TUW': 'TUW', 'TUZ': 'TUZ', 'UNI': 'Uniqa',
+      'WAR': 'Warta', 'WIE': 'Wiener', 'YCD': 'You Can Drive'}
+
+
+
+
+# column = ws.Range(f'AY1:AY{ws.UsedRange.Rows.Count}')
+
+# for i, cash in enumerate(column):
+#       # print(cash)
+#       if cash == 'G':
+#           print(cash)
+
+data_wyst = ExcelApp.Cells(1097, 30).Value
+tow_ub = ExcelApp.Cells(1097, 38).Value
+nr_polisy = ExcelApp.Cells(1097, 40).Value
+inkaso = ExcelApp.Cells(1097, 55).Value
+forma_p = ExcelApp.Cells(1097, 51).Value
+print(data_wyst, nr_polisy, inkaso, forma_p)
+
+
+
+
+
+wb.Close(SaveChanges=False)
+
+
+
 ExcelApp_cash = win32.gencache.EnsureDispatch('Excel.Application')
 ExcelApp_cash.Visible = True
 wb_cash = ExcelApp_cash.Workbooks.Add()
@@ -37,37 +70,14 @@ ws_cash.Cells(1, 4).Value = 'Kwota inkaso'
 
 
 
-tu = {'ALL': 'Allianz', 'AXA': 'AXA', 'COM': 'Compensa', 'EPZU': 'PZU', 'GEN': 'Generali', 'GOT': 'Gothaer',
-      'HDI': 'HDI', 'HES': 'Ergo Hestia', 'IGS': 'IGS', 'INT': 'INTER', 'LIN': 'LINK 4', 'MTU': 'MTU',
-      'PRO': 'Proama', 'PZU': 'PZU', 'RIS': 'InterRisk', 'TUW': 'TUW', 'TUZ': 'TUZ', 'UNI': 'Uniqa',
-      'WAR': 'Warta', 'WIE': 'Wiener', 'YCD': 'You Can Drive'}
+ws_cash.Range('A2:A32').Value = data_wyst.strftime('%Y.%m.%d')
+ws_cash.Range('B2:B32').Value = tu[tow_ub]
+ws_cash.Range('C2:C32').NumberFormat = 0
+ws_cash.Range('C2:C32').Value = nr_polisy
+ws_cash.Range('D2:D32').Value = inkaso
 
 
 
-
-column = ws.Range(f'AY1:AY{ws.UsedRange.Rows.Count}')
-
-for n, cash in enumerate(column):
-      # print(cash)
-      if cash == 'G':
-          print(cash)
-
-          data_wyst = ExcelApp.Cells(n, 30).Value
-          tow_ub = ExcelApp.Cells(n, 38).Value
-          nr_polisy = ExcelApp.Cells(n, 40).Value
-          inkaso = ExcelApp.Cells(n, 55).Value
-          forma_p = ExcelApp.Cells(n, 51).Value
-          print(data_wyst, tu[tow_ub], nr_polisy, inkaso, forma_p)
-
-          ws_cash.Range(f'A2:A{n}').Value = data_wyst.strftime('%Y.%m.%d')
-          ws_cash.Range(f'B2:B{n}').Value = tu[tow_ub]
-          ws_cash.Range(f'C2:C{n}').NumberFormat = 0
-          ws_cash.Range(f'C2:C{n}').Value = nr_polisy
-          ws_cash.Range(f'D2:D{n}').Value = inkaso
-
-
-
-wb.Close(SaveChanges=False)
 
 
 
@@ -75,14 +85,13 @@ ws_cash.Columns.AutoFit()
 ws_cash.Columns(1).ColumnWidth = 12
 ws_cash.Columns(2).ColumnWidth = 10
 
-wb_cash.DisplayAlerts = False
-# cash_ws.DisplayAlerts = False
+wb.DisplayAlerts = False
+# ws_cash.DisplayAlerts = False
 path_do_zapisu_w = r'C:\Users\ROBERT\Desktop\IT\PYTHON\PYTHON 37 PROJEKTY\księgowość\skrypty osobno'
 
-ws_cash.SaveAs(path_do_zapisu_w + "\\inkaso.xlsx")
+wb_cash.SaveAs(path_do_zapisu_w + "\\inkaso.xlsx")
 wb_cash.Close()
 
 
 ExcelApp.Application.Quit()
-ExcelApp_cash.Application.Quit()
 wb_cash.DisplayAlerts = True
