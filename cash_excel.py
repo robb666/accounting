ac145a39a5db6c4be8060467eebe004cdd03d5bf
robb666 <robert.patryk.grzelak@gmail.com>
2021-03-21 11:39:19 +0100

    update

--- cash_excel.py
+++ cash_excel.py
@@ -41,6 +41,8 @@ ws_cash.Cells(1, 1).Value = 'Data'
 ws_cash.Cells(1, 2).Value = 'TU'
 ws_cash.Cells(1, 3).Value = 'Nr polisy'
 ws_cash.Cells(1, 4).Value = 'Kwota inkaso'
+ws_cash.Cells(1, 5).Value = 'Suma inkaso PLN:'
+ws_cash.Cells(1, 5).Font.Bold = True
 
 
 ws.Columns(1).AutoFilter(Field=2, Criteria1=f'21_{m}')
@@ -81,8 +83,21 @@ time.sleep(.6)
 ws.Range(f'BC5:BC{ws.UsedRange.Rows.Count}').Copy()
 time.sleep(.6)
 ws_cash.Range(f'D2').PasteSpecial(Paste=constants.xlPasteValuesAndNumberFormats)
-time.sleep(.6)
 
+for i, value in enumerate(ws_cash.Range(f'D2:D{ws.UsedRange.Rows.Count - col_diff}')):
+
+    if str(value) in ('0.0', 'None', None, ''):
+        ws_cash.Rows(i + 2).EntireRow.Delete()
+
+
+ws_cash.Cells(1, 6).Value = '=SUM(D:D)'
+ws_cash.Cells(1, 6).Font.Size = 15
+ws_cash.Cells(1, 6).Font.Bold = True
+
+xlAscending = 1
+xlSortColumns = 1
+ws_cash.Range(f"A2:D{ws.UsedRange.Rows.Count - col_diff}").Sort(Key1=ws_cash.Range("A1"),
+                                                                Order1=xlAscending, Orientation=xlSortColumns)
 
 ws_cash.Columns.AutoFit()
 ws_cash.Columns(1).ColumnWidth = 11
@@ -92,91 +107,14 @@ path_do_zapisu_w = r'C:\Users\ROBERT\Desktop\IT\PYTHON\PYTHON 37 PROJEKTY\księg
 wb_cash.DisplayAlerts = False
 ExcelApp.Application.CutCopyMode = False
 
-wb_cash.SaveAs(path_do_zapisu_w + "\\inkaso.xlsx")
+okres = (datetime.today() + relativedelta(months=-1)).strftime('%m.%Y')
+wb_cash.SaveAs(path_do_zapisu_w + f"\\gotówka {okres}.xlsx")
 wb.Close(SaveChanges=False)
 wb_cash.Close()
 ExcelApp.Application.Quit()
 ExcelApp_cash.Application.Quit()
 
 wb_cash.DisplayAlerts = True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print(wb.Worksheets(1).Cells(wb.Worksheets(1).Rows.Count, 38).End(-4162).Row)
-# t_ub_range = ws.Range(f'AL5:AL{ws.UsedRange.Rows.Count}').SpecialCells(constants.xlCellTypeVisible).Cells.Count - 39
-# print(t_ub_range)
-# t_ub = ws.Range(f'AL{wb.Worksheets(1).Cells(wb.Worksheets(1).Rows.Count, 38).End(-4162).Row - t_ub_range}:'
-#                 f'AL{ws.UsedRange.Rows.Count - 39}')
-# print(t_ub_range, t_ub)
-
-
-
-
-
-
-# # ws.Columns(1).AutoFilter(Field=51, Criteria1='G')
-# column = ws.Range(f'AY1:AY{ws.UsedRange.Rows.Count}')
-# i = 1
-# j = 2
-# # print(column)
-#
-# # print(ExcelApp.Cells(i, 51).Value)
-# for cash in column:
-#     print(str(cash))
-#     data_wyst = ExcelApp.Cells(i, 30).Value
-#     tow_ub = ExcelApp.Cells(i, 38).Value
-#     nr_polisy = ExcelApp.Cells(i, 40).Value
-#     inkaso = ExcelApp.Cells(i, 55).Value
-#
-#     i += 1
-#     # ExcelApp.Cells(i, 51).Value == 'G'
-#     # if str(cash) == 'G' and str(cash) != 'P':
-#     # str(cash) == 'G' and inkaso is not None and float(inkaso) > 0 and
-#     # and isinstance(data_wyst, datetime)
-#     if (str(cash) == 'G' or str(cash) == 'g') and isinstance(data_wyst, datetime) and inkaso is not None \
-#             and float(inkaso) > 0 and (datetime.today() + relativedelta(months=-2)).strftime('%m') < \
-#                                         datetime.date(data_wyst).strftime('%m') < datetime.today().strftime('%m'):
-#
-#
-#         print(data_wyst.strftime('%Y.%m.%d'), nr_polisy, inkaso, cash)
-#         ws_cash.Cells(j, 1).Value = data_wyst.strftime('%Y.%m.%d')
-#         ws_cash.Cells(j, 2).Value = tu[tow_ub]
-#         ws_cash.Columns(3).NumberFormat = 0
-#         ws_cash.Cells(j, 3).Value = nr_polisy
-#         ws_cash.Cells(j, 4).Value = inkaso
-#         ws_cash.Cells(j, 5).Value = str(cash)
-#
-#         j += 1
-#
-#
-#
-# ws_cash.Columns.AutoFit()
-# ws_cash.Columns(1).ColumnWidth = 12
-# ws_cash.Columns(2).ColumnWidth = 12
-#
-# wb_cash.DisplayAlerts = False
-# # ws_cash.DisplayAlerts = False
-# path_do_zapisu_w = r'C:\Users\ROBERT\Desktop\IT\PYTHON\PYTHON 37 PROJEKTY\księgowość\skrypty osobno'
-#
-# wb_cash.SaveAs(path_do_zapisu_w + "\\inkaso.xlsx")
-# wb.Close(SaveChanges=False)
-# wb_cash.Close()
-# ExcelApp.Application.Quit()
-# ExcelApp_cash.Application.Quit()
-#
-# wb_cash.DisplayAlerts = True
-
 
 
 
