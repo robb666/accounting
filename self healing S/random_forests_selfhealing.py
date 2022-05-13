350b5b592349d9069aa0f950b926c5c406de2ffa
Robb <56496611+robb666@users.noreply.github.com>
2022-05-13 14:58:27 +0200

    up

--- self healing S/random_forests_selfhealing.py
+++ self healing S/random_forests_selfhealing.py
@@ -14,7 +14,7 @@ pd.set_option('display.width', None)
 def scrp(driver):
     html = driver.page_source
     soup = BeautifulSoup(html, 'lxml')
-    tags = ['a']  # to uzupełniać..?
+    tags = ['a', 'input']  # to uzupełniać..?
     # el_name = ['LOGIN', 'PASSW', 'LOG_BUTTON']
     arr = []
     for tag in tags:
@@ -26,30 +26,33 @@ def scrp(driver):
     df = pd.DataFrame.from_records(arr)
     df.insert(0, 'element', df.title)
     df.element.fillna(df['text'], inplace=True)
+    df.element.fillna('DUPA', inplace=True)
     # df.text = np.nan  # fillna didn't work
-    # df = df.replace('\n', '\u2063', regex=True)
-    # print(df)
+    df = df.replace('\n', '\u2063', regex=True)
+    print(df)
     ########
-    # df.to_csv('san.csv', index=False, sep=',', encoding='utf-8')
+    df.to_csv('san.csv', index=False, sep=',', encoding='utf-8')
     # # df = pd.read_csv('san.csv', dtype=object, converters={'some_name':lambda x:x.replace('/n','')})
-    # df = pd.read_csv('san.csv', dtype=object)
+    df = pd.read_csv('san.csv', dtype=object)
 
     return df
 
 
-def healed_locator(driver, e, *, attr, element_row, value):
+def healed_locator(driver, e, *, attr, element_row, value, filename):
     if 'no such element' in str(e) or 'Unable to locate element' in str(e) or 'element not interactable' in str(e):
 
         df = scrp(driver)
-        # df = df.replace('\u2063', '\n', regex=True)
+        # df = pd.read_csv('san.csv')
+        df = df.replace('\u2063', '\n', regex=True)
         df = df.fillna('None')
         # df = df.drop(['Unnamed: 0'], axis=1)
-        print(df.head(15))
+        print(df)
         # df = df.head()
 
         # to_test = pd.read_csv('Test.csv').iloc[[element_row]]
-        to_test = pd.read_csv('Test.csv', dtype=object,
+        to_test = pd.read_csv(filename, dtype=object,
                               header=0, usecols=lambda c: c in df.columns)
+        to_test = to_test.fillna('None')
         to_test = to_test.replace('\u2063', '\n', regex=True)
         print('to_test')
         print(to_test)
@@ -57,6 +60,7 @@ def healed_locator(driver, e, *, attr, element_row, value):
 
         processed_test = pd.concat([df, to_test], axis=0)
         processed_test = processed_test.iloc[[element_row]]
+
 
         print('processed_test')
         print(processed_test)
@@ -95,9 +99,14 @@ def healed_locator(driver, e, *, attr, element_row, value):
             for feature in df.columns:
                 print(f"//*[@{feature}='{el_attr}']")
 
-        # selector = driver.find_element(By.XPATH, f"//*[@{attr}='{el_attr}' and @class='button primary small']")
-        # print(selector)
-        # if value:
-        #     selector.send_keys(value)
-        # else:  # click
-        #     selector.click()
+                try:
+                    selector = driver.find_element(By.XPATH, f"//*[@{feature}='{el_attr}' and @class='button primary small']")
+                    print(selector)
+                    if value:
+                        selector.send_keys(value)
+                    else:  # click
+                        selector.click()
+                    break
+                except Exception as e:
+                    print(e)
+                    pass
