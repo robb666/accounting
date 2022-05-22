02dd8568e357bd2ba20ed40bdde7f4a3e5b1c96a
Robb <56496611+robb666@users.noreply.github.com>
2022-05-22 22:10:50 +0200

    update
    rf selfhealing

---
+++ S_upgraded.py
@@ -0,0 +1,54 @@
+
+from __future__ import print_function
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from faktury_GmailAPI import zsanpl
+import time
+from L_H_ks import url, san_l, san_h
+from random_forests_selfhealing import scrp, healed_locator
+
+
+def san(url):
+    options = webdriver.ChromeOptions()
+    preferences = {'download.default_directory': r'C:\Users\PipBoy3000\Desktop\\'}
+    options.add_experimental_option("prefs", preferences)
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])  # win devtools supress
+    driver = webdriver.Chrome(executable_path=r'M:\zzzProjekty/drivery przegądarek/chromedriver.exe',
+                              options=options)
+    driver.get(url)
+
+    WebDriverWait(driver, 1).until(EC.element_to_be_clickable((
+                                        By.ID, 'privacy-prompt-controls-button-accept'))).click()
+    healed_locator(driver, e=None, attr=None, header=0, helper_attr='', element_row=0, value='')
+    time.sleep(.7)
+
+    helper_attr = "and contains(@class, 'button')"
+    healed_locator(driver, e=None, attr=None, header=0,  helper_attr=helper_attr, element_row=1, value='')
+    WebDriverWait(driver, 1).until(EC.url_changes(url))
+    try:
+        driver.find_element_by_xpath('//div[contains(@id, "button-accept")]').click()
+    except:
+        pass
+    healed_locator(driver, e=None, attr=None, helper_attr='', header=3, element_row=0, value=san_l)
+    healed_locator(driver, e=None, attr=None, helper_attr='', header=3, element_row=1, value='')
+    time.sleep(1)
+    healed_locator(driver, e=None, attr=None, helper_attr='', header=8, element_row=0, value=san_h)
+    healed_locator(driver, e=None, attr=None, helper_attr='', header=8, element_row=1, value='')
+    healed_locator(driver, e=None, attr=None, helper_attr='', header=6, element_row=0, value='')
+
+    time.sleep(3.5)
+
+    tiktok = zsanpl()
+    healed_locator(driver, e=None, attr=None, helper_attr='', header=11, element_row=0, value=tiktok)
+    healed_locator(driver, e=None, attr=None, helper_attr='', header=11, element_row=1, value='')
+    try:
+        time.sleep(1000)
+    except:
+        driver.quit()
+
+
+if __name__ == '__main__':
+    url = url
+    san(url)
---
+++ random_forests_selfhealing.py
@@ -0,0 +1,88 @@
+import time
+
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+
+def scrp(driver):
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    tags = ['a', 'input']  # to uzupełniać..?
+    # el_name = ['LOGIN', 'PASSW', 'LOG_BUTTON']
+    arr = []
+    for tag in tags:
+        for element in soup.find_all(tag):
+            arr.append({'tag': tag} |
+                       {'text': element.text} |
+                       element.attrs)
+
+    df = pd.DataFrame.from_records(arr)
+    df.insert(0, 'element', df.title)
+    df.element.fillna(df['id'], inplace=True)
+    df.element.fillna(df['text'], inplace=True)
+    # df.text = np.nan  # fillna didn't work
+    df = df.replace('\n', '\u2063', regex=True)
+    # print(df)
+    ########
+    # df.to_csv('san.csv', index=False, sep=',', encoding='utf-8')
+    # # df = pd.read_csv('san.csv', dtype=object, converters={'some_name':lambda x:x.replace('/n','')})
+    # df = pd.read_csv('san.csv', dtype=object)
+
+    return df
+
+
+def healed_locator(driver, e, *, attr=None, helper_attr, header, element_row, value, filename='Test.csv'):
+    df = scrp(driver)
+    df = df.fillna('None')
+    df = df.replace('\u2063', '\n', regex=True)
+
+    to_test = pd.read_csv(filename, dtype=object, header=header,
+                          usecols=lambda c: c in df.columns).iloc[[element_row]]
+
+    to_test = to_test.fillna('None')
+    to_test = to_test.replace('\u2063', '\n', regex=True)
+
+    processed_test = pd.concat([df, to_test], axis=0)
+    print(processed_test)
+
+    processed_test = processed_test.iloc[[-1]]
+
+    ohe = OneHotEncoder(sparse=False, handle_unknown='ignore')
+    X_train = ohe.fit_transform(df.astype(str))
+    X_test = ohe.transform(processed_test)
+
+    element_dict = dict(zip(df['element'].unique(), range(df['element'].nunique())))
+    y_train = df['element'].replace(element_dict)
+
+    rf = RandomForestClassifier(n_estimators=50, random_state=0)
+    rf.fit(X_train, y_train)
+
+    probabilities = rf.predict_proba(X_test)[0]
+    print(probabilities)
+    el_attr = list(element_dict.keys())[np.argmax(probabilities)]
+
+    columns = df.columns[df.isin([el_attr]).any()].values  # kolumny atrybutu
+    # TODO zakwalifikować atrybut..bez iteracji
+    for attr in columns[1:]:
+        print( f"//*[@{attr}='{el_attr}' {helper_attr}]")
+        try:  # kiedy więcej niż jeden element o danym atrybucie znajduje się na stronie.
+            selector = driver.find_element(By.XPATH, f"//*[@{attr}='{el_attr}' {helper_attr}]")
+            if value:
+                selector.send_keys(value)
+            else:  # click
+                WebDriverWait(driver, 5).until(EC.element_to_be_clickable((
+                    By.XPATH, f"//*[@{attr}='{el_attr}' {helper_attr}]"))).click()
+            break
+        except Exception as e:
+            print(e)
