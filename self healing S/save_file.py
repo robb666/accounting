6999719eeb904048f5cc3b8c45de6e20a58898ad
Robb <56496611+robb666@users.noreply.github.com>
2022-05-25 18:47:57 +0200

    opis
    store.append('store_key16', df_pickle, format='fixed', append=False)

--- self healing S/save_file.py
+++ self healing S/save_file.py
@@ -1,11 +1,51 @@
+import os
+
 import pandas as pd
 import numpy as np
+import string
+import io
 
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', 120)
+
+
+df = pd.DataFrame({
+     'int32':    [['ada', 'ala'], ['ula'], None, None],
+     'int64':    np.random.randint(10**7, 10**9, 4).astype(np.int64)*10,
+     'float':    np.random.rand(4),
+     'string':   np.random.choice(['jed', 'dwa', None, None], 4),
+     'string2':   np.random.choice(['jed', 'dwa', None, None], 4),
+     })
+
+# print(df)
+
+
+df_pickle = pd.read_pickle('san.pkl')[:]
+# df_pickle = pd.DataFrame(df_pickle)
+print(df_pickle)
+
+
+store = pd.HDFStore('example.h5')
+
+
+# store.append('store_key16', df_pickle, format='fixed', append=False)
+
+# print(store.info())
+# store.close()
+
+# df = pd.read_hdf(store, key='store_key16')
+# store.close()
+# print(df)
+
+
+
+
+
+
+
 
 
 
@@ -18,11 +58,11 @@ pd.set_option('display.max_colwidth', 120)
 #
 # df = pd.DataFrame(df['class'], columns=('I', 'Aas'))# put the dataset in the storage
 # hdf.put('d1', df, format='table')
-# hdf.close()
+
 #
-# df1 = pd.read_hdf('storage.h5', key='d1', format='table')
+# df1 = hdf.get_storer('san1')
 # print()
-# print(hdf['df1'])
+# print(df1)
 
 
 
@@ -39,6 +79,7 @@ pd.set_option('display.max_colwidth', 120)
 
 # df = pd.read_csv('san.csv')
 # df = pd.read_pickle('san.pkl')
+# print(df)
 # df = pd.read_hdf('storage.h5', key='san1')
 # df = pd.read_csv('Test.csv', header=16, on_bad_lines='skip')
 
