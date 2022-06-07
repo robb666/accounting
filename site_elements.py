9735e3e00c72c38281db1b130d3191efaf233e88
Robb <56496611+robb666@users.noreply.github.com>
2022-06-07 20:14:56 +0200

    Update site_elements.py

--- site_elements.py
+++ site_elements.py
@@ -1,17 +1,61 @@
 import pandas as pd
+import numpy as np
 import os
 import sys
+import h5py
 import subprocess
+# store = pd.HDFStore(os.getcwd() + '\elements.h5')
 
-# os.environ['PATH'] += \
-#     os.pathsep + os.path.expanduser(r'C:\Users\PipBoy3000\Desktop\IT\projekty\accounting\.env\Lib\site-packages\tables.libs')
+
+
+# # store = h5py.File('elements.h5', 'r')
+with h5py.File('elements.h5') as hdf:
+    ls = list(hdf.keys())
+    print(f'List of datasets: {ls}')
+    data = list(hdf.items())
+    print(list(data[0][1].get('axis0')[:]))
+    # data = hdf.get('nik')
+    # print(list(data['axis0']))
+    # print(list(data['axis1']))
+    # print(list(data['block0_items']))
+    # print(list(data['block0_values']))
+    # print(list(data))
+
+    # print(list(hdf))
+    # print(list(hdf.get('nik')))
+    # base_items = list(hdf.items())
+    # print(base_items)
+    # G2 = hdf.get('ordinarypin')
+    # G2_items = list(G2.items())
+    # print(G2_items)
+    # G21 = G2.get('axis0')
+    # dataset = list(G21)
+    #
+    # print(dataset)
+    #
+    # df = pd.DataFrame(dataset).T
+    # print(df)
+    # Gn = G2.get('zaloguj')
+    # print(Gn)
+
+    # print(Gn)
+# for i in store:
+#     print(store[i])
+
+
+
+
+print()
+print()
+print()
+print()
+
+
 
 
 
 store = pd.HDFStore(os.getcwd() + '\elements.h5')
-# print(store.info())
-
-
+print(store.info())
 class Elements:
     accept_1 = pd.read_hdf(store, key='accept_1')
     login = pd.read_hdf(store, key='zaloguj')
@@ -26,8 +70,6 @@ class Elements:
     otp_button = pd.read_hdf(store, key='otp_button')
 
     store.close()
-
-
 os.getcwd()
 el = Elements()
-print(el.accept_1)
+print(el.nik)
